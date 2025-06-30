9c5ee4aaab41dcef7c2712cd15d53cd3878f9394
chabeerabsal <chabeerabsal@gmail.com>
2025-06-30 08:10:02 +0530

    RestController

---
+++ Day1-RestController/src/main/java/com/example/Day1_RestController/Day1RestControllerApplication.java
@@ -0,0 +1,14 @@
+package com.example.Day1_RestController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1RestControllerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Day1RestControllerApplication.class, args);
+	}
+
+}
