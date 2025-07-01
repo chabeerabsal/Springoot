b688cfe1a9f54bac4b563afa18d6e7a97bdf22b5
chabeerabsal <chabeerabsal@gmail.com>
2025-07-01 08:26:38 +0530

    Spring data JPA

---
+++ Day2-SpringdataJPA/src/main/java/com/example/Day2_SpringdataJPA/Day2SpringdataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Day2_SpringdataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2SpringdataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day2SpringdataJpaApplication.class, args);
+	}
+
+}
---
+++ Day2-SpringdataJPA/src/test/java/com/example/Day2_SpringdataJPA/Day2SpringdataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Day2_SpringdataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day2SpringdataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
