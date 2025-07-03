d01e299fda1d8853ff61cca31deff4960645b998
chabeerabsal <chabeerabsal@gmail.com>
2025-07-03 09:14:55 +0530

    Pagination

---
+++ Day4-Pagination/src/main/java/com/example/Day4_Pagination/Day4PaginationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Day4_Pagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day4PaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day4PaginationApplication.class, args);
+	}
+
+}
