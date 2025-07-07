408169abade440470849f0b197ca6999acf62804
chabeerabsal <chabeerabsal@gmail.com>
2025-07-07 10:01:41 +0530

    Spring Security

---
+++ Day4-Spring-Security/src/main/java/com/example/Day4_Spring/Security/Day4SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.Day4_Spring.Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day4SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day4SpringSecurityApplication.class, args);
+	}
+
+}
