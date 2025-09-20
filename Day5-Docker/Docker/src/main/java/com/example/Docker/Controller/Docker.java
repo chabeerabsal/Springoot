5fb9f99661daf920cd6eebba601b0e6edfeeeba2
chabeerabsal <chabeerabsal@gmail.com>
2025-09-20 22:43:27 +0530

    docker continued

--- Day5-Docker/Docker/src/main/java/com/example/Docker/Controller/Docker.java
+++ Day5-Docker/Docker/src/main/java/com/example/Docker/Controller/Docker.java
@@ -1,4 +1,15 @@
 package com.example.Docker.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Docker {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
 }
