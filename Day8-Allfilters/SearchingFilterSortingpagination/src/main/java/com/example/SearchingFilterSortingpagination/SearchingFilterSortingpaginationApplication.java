9ec9aff613d1cfdf264e702d5f3af65476179601
chabeerabsal <chabeerabsal@gmail.com>
2025-11-12 22:44:11 +0530

    SearchingFiltering

---
+++ Day8-Allfilters/SearchingFilterSortingpagination/src/main/java/com/example/SearchingFilterSortingpagination/SearchingFilterSortingpaginationApplication.java
@@ -0,0 +1,13 @@
+package com.example.SearchingFilterSortingpagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchingFilterSortingpaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchingFilterSortingpaginationApplication.class, args);
+	}
+
+}
---
+++ Day8-Allfilters/SearchingFilterSortingpagination/src/test/java/com/example/SearchingFilterSortingpagination/SearchingFilterSortingpaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SearchingFilterSortingpagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchingFilterSortingpaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
