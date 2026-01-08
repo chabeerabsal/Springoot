d503f37be176d190d5f0dc1674d3d6612c614138
chabeerabsal <chabeerabsal@gmail.com>
2026-01-08 20:31:10 +0530

    added

--- Streams/src/Main.java
+++ Streams/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-       List<Integer> list = new ArrayList<Integer>(Arrays.asList(1,12,23,14,5,5));
+//       List<Integer> list = new ArrayList<Integer>(Arrays.asList(1,12,23,14,5,5));
 //
 ////       List<Integer> list1=list.stream().filter((Integer i) -> i % 2 == 0).collect(Collectors.toList());
 ////       System.out.println(list1);
@@ -25,30 +25,32 @@ public class Main {
 //
 //        System.out.println(duplicates);
 
-        Optional<Integer>min=list.stream().min((Integer i,Integer j)->i-j);
-        Optional<Integer> max=list.stream().max((Integer i,Integer j)->i-j);
+//        Optional<Integer>min=list.stream().min((Integer i,Integer j)->i-j);
+//        Optional<Integer> max=list.stream().max((Integer i,Integer j)->i-j);
 
 //        System.out.println(min.get());
 //        System.out.println(max.get());
 //           List<String> s=Arrays.asList("Apple", "Ant", "Dog", "Air", "Cat");
 //           s.stream().filter(i->i.startsWith("A")).forEach(System.out::println);
 
-           List<Integer> list1=Arrays.asList(1,12,23,1,5,5);
-
-           list1.stream().collect(Collectors.groupingBy(i->i,()->new LinkedHashMap<>(),Collectors.counting())).entrySet()
-                   .stream().filter(entry->entry.getValue()>1).map(Map.Entry::getKey).findFirst().ifPresent(System.out::println);
+//           List<Integer> list1=Arrays.asList(1,12,23,1,5,5);
+//
+//           list1.stream().collect(Collectors.groupingBy(i->i,()->new LinkedHashMap<>(),Collectors.counting())).entrySet()
+//                   .stream().filter(entry->entry.getValue()>1).map(Map.Entry::getKey).findFirst().ifPresent(System.out::println);
 //        String ch="chabeer";
 //        ch.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(c->c,Collectors.counting())).entrySet().stream().forEach(e->System.out.println(e.getKey()+"+"+e.getValue()));
 //
 
         List<Employee> employeeList=new ArrayList<>(Arrays.asList(
                 new Employee("Alice",1000),
+                new Employee("Bob",2000),
                 new Employee("Alice",2000),
                 new Employee("Alice",3000)
                 )
         );
 
-        employeeList.stream().map(e->e.getSalary()).sorted(Comparator.reverseOrder()).peek(System.out::println).collect(Collectors.toList());
+       List<Integer>s= employeeList.stream().map(Employee::getSalary).sorted(Comparator.reverseOrder()).distinct().peek(System.out::println).toList();
+       System.out.println(s.get(1));
 
     }
 
